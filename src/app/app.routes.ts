d29932724b6a2d90f2226816b204f28de6bd12c6
Abel Borit <abelboritguitton@gmail.com>
2025-01-16 16:04:44 -0500

    [update] app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,31 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import {
+  InputComponent,
+  OutputComponent,
+  QueriesComponent,
+} from './pages/signals';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+
+  {
+    path: 'signals',
+    children: [
+      {
+        path: 'inputs',
+        component: InputComponent,
+      },
+      {
+        path: 'outputs',
+        component: OutputComponent,
+      },
+      {
+        path: 'queries',
+        component: QueriesComponent,
+      },
+    ],
+  },
+
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ src/app/pages/signals/index.ts
@@ -0,0 +1,5 @@
+export * from './input/input.component';
+export * from './input-child/input-child.component';
+export * from './output/output.component';
+export * from './output-child/output-child.component';
+export * from './queries/queries.component';
