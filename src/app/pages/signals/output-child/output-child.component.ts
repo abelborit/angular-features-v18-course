91256cb1ba7c61aec28b464688325a677b9651eb
Abel Borit <abelboritguitton@gmail.com>
2025-01-22 13:52:45 -0500

    [update] output.component & output-child.component

--- src/app/pages/signals/output-child/output-child.component.ts
+++ src/app/pages/signals/output-child/output-child.component.ts
@@ -1,13 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-output-child',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './output-child.component.html',
   styleUrl: './output-child.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OutputChildComponent {
+  /* darse cuenta que el "sumar" tiene el output que manejará el tipo "void" es por eso que en -- (sumar)="handleSumar()" -- de "output.component.html" no se coloca el "$event" en cambio en el de "delete" estará manejando un tipo "number" porque se le está pasando "0" para que siempre se reinicie a "0" */
+  sumar = output<void>();
+  delete = output<number>();
 
+  enviarSuma() {
+    /* está emitiendo un valor void */
+    this.sumar.emit();
+  }
+
+  reset() {
+    /* está emitiendo un valor 0 */
+    this.delete.emit(0);
+  }
 }
--- src/app/pages/signals/output/output.component.ts
+++ src/app/pages/signals/output/output.component.ts
@@ -1,13 +1,39 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { OutputChildComponent } from '../output-child/output-child.component';
 
 @Component({
   selector: 'app-output',
   standalone: true,
-  imports: [],
+  imports: [
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+    OutputChildComponent,
+  ],
   templateUrl: './output.component.html',
   styleUrl: './output.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OutputComponent {
+  badge = signal<number>(0);
 
+  hidden = false;
+
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
+
+  handleSumar() {
+    this.badge.update((value: number) => value + 1);
+  }
+
+  handleDelete(value: number) {
+    /* aquí está el valor emitido desde el hijo que sería el valor de 0 */
+    // console.log(value);
+
+    this.badge.update(() => value);
+  }
 }
