201c92a95ab1a5354fbad44696edb33d9bffbc59
Abel Borit <abelboritguitton@gmail.com>
2025-01-22 14:09:31 -0500

    [update] queries.component

--- src/app/pages/signals/queries/queries.component.ts
+++ src/app/pages/signals/queries/queries.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  viewChild,
+  viewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-queries',
@@ -6,8 +12,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './queries.component.html',
   styleUrl: './queries.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QueriesComponent {
+  h1 = viewChild<ElementRef<HTMLHeadingElement>>('titulo'); // es un "HTMLHeadingElement" porque es un "h1" en este caso
+  h2 = viewChild.required<ElementRef<HTMLHeadingElement>>('subtitulo'); // es un "HTMLHeadingElement" porque es un "h1" en este caso
+  soloLectura = viewChildren<ElementRef<HTMLHeadingElement>>('soloLectura');
 
+  ngAfterViewInit() {
+    this.h2().nativeElement.textContent = 'Nuevo valor asignado al subtitulo';
+
+    console.log(this.h1()?.nativeElement);
+    console.log(this.h2()?.nativeElement);
+    console.log(this.soloLectura());
+  }
 }
