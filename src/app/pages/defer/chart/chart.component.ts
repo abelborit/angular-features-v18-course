88e4127c996ee4f0addf87ce1c73c2b2cb8881a5
Abel Borit <abelboritguitton@gmail.com>
2025-01-22 22:18:14 -0500

    [update] defer.component & chart.component

--- src/app/pages/defer/chart/chart.component.ts
+++ src/app/pages/defer/chart/chart.component.ts
@@ -1,13 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartComponent {
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+    datasets: [
+      {
+        data: [75, 70, 80, 81, 75, 90],
+        label: 'Efectividad Equipo Desarrollo',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)',
+      },
+    ],
+  };
 
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: false,
+  };
+
+  public lineChartLegend = true;
 }
--- src/app/pages/defer/defer.component.ts
+++ src/app/pages/defer/defer.component.ts
@@ -1,13 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChartComponent } from './chart/chart.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-defer',
   standalone: true,
-  imports: [],
+  imports: [
+    ChartComponent,
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
   templateUrl: './defer.component.html',
   styleUrl: './defer.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeferComponent {
-
+  isVisible = signal(false);
 }
